0e359cb1411def421d6fd8ea27fc24f06dcd12f9
maerd-zinbieL <db1345569965@gmail.com>
2025-06-16 20:57:06 +0800

    wip

--- code/1.1.EvaluatingExpressions.lean
+++ code/1.1.EvaluatingExpressions.lean
@@ -1,8 +1,8 @@
-//https://lean-lang.org/functional_programming_in_lean/Getting-to-Know-Lean/Evaluating-Expressions/#evaluating
-3#eval 1 + 2
-11#eval 1 + 2 * 5
-"Hello, Lean!"#eval String.append "Hello, " "Lean!"
-"great oak tree"#eval String.append "great " (String.append "oak " "tree")
+-- https://lean-lang.org/functional_programming_in_lean//Getting-to-Know-Lean/Evaluating-Expressions/#evaluating
+#eval 1 + 2
+#eval 1 + 2 * 5
+#eval String.append "Hello, " "Lean!"
+#eval String.append "great " (String.append "oak " "tree")
 String.append "it is " (if 1 > 2 then "yes" else "no")
 String.append "it is " (if false then "yes" else "no")
 String.append "it is " "no"
@@ -10,5 +10,4 @@ String.append "it is " (if 1 > 2 then "yes" else "no")
 String.append "it is " (if false then "yes" else "no")
 String.append "it is " "no"
 "it is no"
-could not synthesize a 'ToExpr', 'Repr', or 'ToString' instance for type
-  String → String#eval String.append "it is "
+#eval String.append "it is "
--- crawler.ts
+++ crawler.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { JSDOM } from 'jsdom';
-import * as fs from 'fs/promises';
+import * as cheerio from 'cheerio';
+import fs from 'fs/promises';
 
 const ROOT_CONTENTS_SELECTOR = "body > div > main > div > section > section > ol > li > a"
 const CHAPTER_CONTENTS_SELECTOR = "body > div > main > div > section > ol > li > a"
@@ -16,21 +16,21 @@ async function parse() {
       const codes = await parseSectionPage(section);
       await randomDelay(1000, 3000);
       saveCode(section, codes);
-      return;
     }
   }
 }
 
 function randomDelay(min: number, max: number) {
-    const ms = Math.random() * (max - min) + min;
-    return new Promise(resolve => setTimeout(resolve, ms));
+  const ms = Math.random() * (max - min) + min;
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function checkFileExists(filePath: string): Promise<boolean> {
   try {
-    await fs.access(filePath, fs.constants.F_OK);
+    await fs.access(filePath);
     return true;
   } catch (error) {
+    console.log(error)
     return false;
   }
 }
@@ -64,10 +64,13 @@ async function parsePageNameUrls(nameUrl: NameUrl, selector: string) {
     page = await getPage(nameUrl.url);
     fs.writeFile(filePath, page, 'utf8');
   }
-  const dom = new JSDOM(page);
-  const document = dom.window.document;
-  const aTags = document.querySelectorAll(selector) as NodeListOf<HTMLAnchorElement>;
-  const contentsHref = Array.from(aTags).map(it => ({ name: it.text.replace(/\s/g, ''), url: `${ROOT_URL}${it.href}` } as NameUrl))
+  const contentsHref = [] as NameUrl[]
+  const $ = cheerio.load(page);
+  const aTags = $(selector);
+  aTags.each((idx, element) => {
+    const aTag = $(element)
+    contentsHref.push({ name: aTag.text().replace(/\s/g, ''), url: `${ROOT_URL}/${aTag.attr('href') || ''}` })
+  })
   return contentsHref;
 }
 
@@ -88,18 +91,23 @@ async function parseSectionPage(nameUrl: NameUrl) {
     page = await fs.readFile(filePath, 'utf8');
   } else {
     page = await getPage(nameUrl.url);
-    writeFile(filePath, `${nameUrl.url}\n${page}`);
+    writeFile(filePath, page);
   }
-  const dom = new JSDOM(page);
-  const document = dom.window.document;
-  const codeTags = document.querySelectorAll('code.hl.lean.block');
-  const codes = Array.from(codeTags).map(it => it.textContent || '')
+  const codes = [] as string[]
+  const $ = cheerio.load(page);
+  const codeTags = $('code.hl.lean.block');
+  codeTags.each((idx, element) => {
+    let codeTag = $(element)
+    codeTag.find('.hover-container').remove()
+    codes.push(codeTag.text())
+  })
   return codes;
+  // return codes.filter(it => !it.trim().startsWith('#'));
 }
 
 async function saveCode(nameUrl: NameUrl, codes: string[]) {
   const filePath = `./code/${nameUrl.name}.lean`;
-  const content = `//${nameUrl.url}\n${codes.join("\n")}`
+  const content = `-- ${nameUrl.url}\n${codes.join("\n")}`
   writeFile(filePath, content);
 }
 parse()
